ffb5b28ddf4c0a3a3df5f28a41ff8447772affa9
Mehmet Oguz <mehmetoguz@Mehmets-MacBook-Pro.local>
2022-07-06 15:50:52 +0300

    Update spec factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :open_graph_entity do
-    title { Faker::Lorem.sentence }
+    title       { Faker::Lorem.sentence }
+    url         { Faker::Internet.url }
     description { Faker::Lorem.paragraph }
   end
 end
